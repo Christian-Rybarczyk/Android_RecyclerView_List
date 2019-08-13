be13b3d65290fa18b8b15132bba9860a71b75b9f
Christian Rybarczyk <christian.j.rybarczyk@gmail.com>
2019-08-13 18:03:12 -0400

    displayed images in recyclerview

--- app/src/main/java/com/rybarstudios/favoritepicturesgallery/activity/MainActivity.kt
+++ app/src/main/java/com/rybarstudios/favoritepicturesgallery/activity/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.rybarstudios.favoritepicturesgallery.activity
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -11,9 +13,10 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
 
     var imageList: ArrayList<ImageData> = ArrayList()
+    var adapter: RecyclerViewAdapter? = null
 
     companion object {
-        const val IMAGE_REQUEST_CODE = 654321
+        const val IMAGE_REQUEST_CODE = 65
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,12 +25,34 @@ class MainActivity : AppCompatActivity() {
 
         imageList = ArrayList()
 
-        
+        add_image_button.setOnClickListener {
+            val intent = Intent(Intent.ACTION_GET_CONTENT)
+            intent.type = "image/*"
+            if (intent.resolveActivity(packageManager) != null) {
+                startActivityForResult(intent, IMAGE_REQUEST_CODE)
+            }
+        }
 
         val manager = LinearLayoutManager(this)
         recycler_view.layoutManager = manager
 
-        val adapter = RecyclerViewAdapter(imageList)
+        adapter = RecyclerViewAdapter(imageList)
         recycler_view.adapter = adapter
+    }
+
+    private fun refreshListView() {
+        adapter!!.notifyDataSetChanged()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK && requestCode == IMAGE_REQUEST_CODE) {
+            if (data != null) {
+                val imageUri = data.data
+                imageList.add(ImageData(imageUri!!))
+                adapter!!.notifyItemInserted(imageList.size - 1)
+            }
+        }
+        refreshListView()
     }
 }
